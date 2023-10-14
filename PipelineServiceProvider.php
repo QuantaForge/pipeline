b4a1e9b8e6727afb8b5e8bcac7d3b3fd7d1d6206
Tomáš Selič (Smisch-DEV) <contact@smisch.sk>
2023-10-14 16:41:25 +0200

    v1.0.0

---
+++ PipelineServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace QuantaForge\Pipeline;
+
+use QuantaForge\Contracts\Pipeline\Hub as PipelineHubContract;
+use QuantaForge\Contracts\Support\DeferrableProvider;
+use QuantaForge\Support\ServiceProvider;
+
+class PipelineServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            PipelineHubContract::class,
+            Hub::class
+        );
+
+        $this->app->bind('pipeline', fn ($app) => new Pipeline($app));
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            PipelineHubContract::class,
+            'pipeline',
+        ];
+    }
+}
